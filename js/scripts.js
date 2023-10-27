e4a79d03f86695cca53d7d79b1d136a1dccf1d04
ynasss <radoulova.alexandra@gmail.com>
2023-10-27 20:21:27 +0200

    add js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Hello Carlo!")
+function menuToggle () {
+    var x = document.getElementById("myNavtoggle");
+    if (x.className === "navtoggle") {
+        x.className += " responsive";
+    }
+    else {
+        x.className = "navtoggle"
+    }
+}
