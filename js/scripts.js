bb05888107937865ce74841e3cd6e3f4142d295d
ynasss <radoulova.alexandra@gmail.com>
2023-10-29 11:58:25 +0100

    write code for button toggle

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,20 @@ function menuToggle () {
         x.className = "navtoggle"
     }
 }
+$(document).ready(function() {
+$(".toggle-button").click(function() {
+    var combineId = $(this).data("id");
+    var textButton = $(this).text();
+
+    $("#" + combineId).toggle();
+
+    if (textButton === $(this).data("read-more")) {
+        $(this).text($(this).data("read-less"));
+    }
+    else {
+        $(this).text($(this).data("read-more"));
+    }
+
+
+});
+});
